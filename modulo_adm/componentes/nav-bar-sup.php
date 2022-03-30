00053108c569e185ed07568d4875ac1599cedc5c
maylonho <oliveira_maylon@hotmail.com>
2022-03-30 17:54:41 -0300

    m

--- modulo_adm/componentes/nav-bar-sup.php
+++ modulo_adm/componentes/nav-bar-sup.php
@@ -10,7 +10,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-        </li>
+        </li> 
       </ul>
       <form class="d-flex" action="../php/logoff.php">
         <labe>Olá <?php echo $_SESSION['usuario_logado']; ?></label>
